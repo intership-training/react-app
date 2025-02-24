f024a96910f2a9ba7e2e52658830a43cc9f71aa5
kanom99life <teeraphat.first@gmail.com>
2025-02-24 14:56:37 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import { ColDef } from 'ag-grid-community';
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import './App.css';
 
-function App() {
+type CarType = {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+};
+
+
+function App(){
+  const rowData: CarType[] = [
+    { 
+      make: "Tesla", 
+      model: "Model Y", 
+      price: 64950, 
+      electric: true 
+    },
+    { 
+      make: "Ford", 
+      model: "F-Series", 
+      price: 33850, 
+      electric: false 
+    },
+    { 
+      make: "Toyota", 
+      model: "Corolla", 
+      price: 29600, 
+      electric: false },
+  ];
+
+  const colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className={"ag-theme-quartz-dark"} 
+      style={{ height: 500}}
+    >
+      <AgGridReact 
+        rowData={rowData}
+        columnDefs={colDefs}
+      />
     </div>
   );
 }
 
 export default App;
+// Render GridExample
